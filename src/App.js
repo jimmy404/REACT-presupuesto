e0ec66a0fdba335124ae49f65855f73f0787f8c7
Jimmy <bruno_pier@hotmail.com>
2019-12-09 19:42:19 -0300

    Adding components file - Adding Pregunta.js - 1st steps

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import Pregunta from './components/Pregunta'
 
 function App() {
   return (
     <div className="App container">
+      <header>
       <h1>Gasto Semanal</h1>
+      <div className="contenido-principal contenido">
+        <Pregunta />
+      </div>
+      </header>
     </div>
   );
 }
