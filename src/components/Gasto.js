b7eec62eebe45e846abf6f1c4f39c76034faa7a0
Jimmy <bruno_pier@hotmail.com>
2019-12-10 01:21:02 -0300

    Adding Gasto.js file - work inside

---
+++ src/components/Gasto.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Gasto = () => (
+    <li className="gastos">
+        <p>
+            {Gasto.nombreGasto}
+            <span className="gasto">${Gasto.cantidadGasto}</span>
+        </p>
+    </li>
+);
+
+export default Gasto;
